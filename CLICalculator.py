abb3cf1b1c9b99642211a9e56ef69a3d8acf44ae
Eno.py <essieneno10@gmail.com>
2025-02-05 09:26:35 +0100

    First project

---
+++ CLICalculator.py
@@ -0,0 +1,30 @@
+while True:
+    num1 = int(input("Enter the first number: "))
+    num2 = ""
+    operator = ""
+    
+    if (num1 / 1 == num1):
+        num2 = int(input("Enter the second number: "))
+    else:
+        print("Invalid input")
+    if (num2 / 1 == num2):
+        operator = input("Enter an operator: ")
+    else:
+        print("Invalid input")
+
+    
+    if (operator == "add"):
+        print(num1 + num2)
+    elif (operator == "subtract"):
+        print(num1 - num2)
+    elif (operator == "multiply"):
+        print(num1 * num2)
+    elif (operator == "divide"):
+        print (num1 / num2)
+    else: 
+        print("invalid opeartor")
+    
+    answer = input("Would you like to continue? ").strip().lower()
+    if (answer == "no"):
+        print("Thank you for using the calculator")
+        break
