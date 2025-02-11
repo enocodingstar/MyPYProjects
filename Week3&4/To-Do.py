f897237f07ba9b9ec352e7922631a2baf7d0d40e
Eno.py <essieneno10@gmail.com>
2025-02-11 19:48:00 +0100

    First commit

---
+++ Week3&4/To-Do.py
@@ -0,0 +1,87 @@
+import ToDoFunctions
+
+def main():
+    filename = "Task_Records.txt"
+    ToDoFunctions.loadfile(filename)
+
+    """Welcome Messages"""
+    print("Welcome to the To-Do App.\n What would you like to do today? \n Choose from the following options: \n")
+
+    while True:
+        print("1. Add a task")
+        print("2. View all tasks")
+        print("3. Mark tasks as complete")
+        print("4. Remove tasks")
+        print("5. Exit")
+
+        choice = input("Enter your choice (1-5): ")
+
+        if (choice == "1"):
+            print("Add a task: ")
+            name = input("Enter the task name: ")
+            description = input("Describe the task: ")
+            deadline = input("Enter the deadline for the task in hours")
+            status = "incomplete"
+
+            task = ToDoFunctions.addTasks(name, description, deadline, status)
+
+            content = f"Task {task["name"]} has been added"
+            ToDoFunctions.appendRecords(filename, content)
+
+            print(content)
+        elif choice == "2":
+            tasks = ToDoFunctions.viewTasks()
+
+            if tasks:
+                for i, task in enumerate(tasks, start=1):
+                    print(f"{i}. Name: {task["name"]} \n  Description: {task["description"]} \n Deadline: {task["deadline"]} hours \n Status: {task["status"]}")
+            else:
+                print("No tasks to show.")
+        elif (choice == "3"):
+            name = input("Enter the task name: ")
+
+            task = ToDoFunctions.markTasks(name)
+
+            if task:
+                if task["status"] == "incomplete":
+                    task["status"] = "complete"
+                    content = f"Congratulations! Task '{task["name"]}' has been completed."
+                    ToDoFunctions.appendRecords(filename, content)
+
+                    print(content)
+                elif task["status"] == "complete":
+                    print("Task already completed")
+                else:
+                    print("Task doesn't exist.")
+        elif (choice == "4"):
+            tasks = ToDoFunctions.viewTasks()
+
+            if tasks:
+                for i, task in enumerate(tasks, start=1):
+                    print(f"{i}. Name: {task["name"]} \n  Description: {task["description"]} \n Deadline: {task["deadline"]} hours \n Status: {task["status"]}")
+                index = int(input("Enter a task number: "))
+                removedTask = ToDoFunctions.removeTasks(index)
+                print(f"Task {removedTask["name"]} has been removed.")
+            else:
+                print("No tasks to show.")
+        elif (choice == "5"):
+            print("Thanks for using the To-Do App \n See you next time!")
+            break
+        else:
+            print("Invalid input")
+            
+            
+
+
+
+
+
+
+
+
+
+
+
+
+if (__name__ == "__main__"):
+    main()
---
+++ Week3&4/ToDoFunctions.py
@@ -0,0 +1,50 @@
+"""Storing records in a file"""
+def loadfile(filename):
+    try:
+        with open(filename, "w") as file:
+            file.write("All activities are recorded and stored here.\n")
+    except FileExistsError:
+        print(f"File {filename} already exists")
+    return[]
+
+"""Appending a record to the file"""
+def appendRecords(filename, content):
+    try:
+        with open(filename, "a") as file:
+            file.write(content + "\n")
+    except Exception as e:
+        print("An error occured")
+    return filename, content
+
+
+global tasks
+tasks = []
+
+
+"""Add tasks"""
+def addTasks(name, description, deadline, status):
+    task = {"name": name, "description": description, "deadline": deadline, "status": status}
+    tasks.append(task)
+    return task
+
+
+"""View tasks"""
+def viewTasks():
+    return tasks
+
+
+"""Mark task as complete"""
+def markTasks(name):
+    for task in tasks:
+        if task["name"] == name:
+            return task
+        return None
+
+"""Removing tasks"""
+def removeTasks(index):
+        removedTask = tasks.pop(index)
+        return removedTask
+
+
+
+            
