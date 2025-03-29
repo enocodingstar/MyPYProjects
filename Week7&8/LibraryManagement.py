720838150493c3041e1c5b114d1f13f9869101e0
Eno.py <essieneno10@gmail.com>
2025-03-29 22:06:33 +0100

    Saving some changes

--- Week7&8/LibraryManagement.py
+++ Week7&8/LibraryManagement.py
@@ -1,6 +1,7 @@
 users = []
 books = []
 
+
 class User:
     def __init__(self, name):
         self.name = name
@@ -11,6 +12,8 @@ class User:
                 print("User already exists!")
             else:
                 users.append(self.name)
+                print(users)
+                print(f"User {self.name} has been added.")
 
 class Book:
     def __init__(self, title, author, status, duedate, fine):
@@ -25,7 +28,11 @@ class Book:
             if self.title == book["title"] & self.author == book["author"]:
                 print("Book already exists")
             else:
-                books.append()
+                book = {"title": self.title, "author": self.author, "status": self.status}
+                books.append(book)
+        
+
+
 
 
 
@@ -43,4 +50,27 @@ while True:
           "1. Add a user \n"
           "2. View users\n"
           "3. Add a book\n"
-          "4. View books\n")                
+          "4. View books\n"
+          "5. Borrow a book \n"
+          "6. Return a book\n"
+          "7.Remove a user\n"
+          "8. Remove a book\n"
+          "9. Exit the library")
+
+    try:
+        choice = int(input("Enter your choice (1-5): "))
+    except ValueError:
+        print("Invalid choice. Enter a valid choice (1-5)")
+    else:
+        if choice == 1:
+            username = input("Enter the username: ")
+            user = User(username)
+            user.addUser()
+            print(f"User {username} has been added.")
+        elif choice == 2:
+            if users:
+                for i, user, in enumerate(users, start=1):
+                    print(f"{i}. {user.name} ")
+            else:
+                print("No users to show.")
+
